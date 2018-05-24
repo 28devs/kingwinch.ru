f5739da5b09e96700266f4594524ec82e0e41ee0
Yuriy Krugoviy <CircularBoy@users.noreply.github.com>
2018-05-24 12:02:05 +0300

    auth registration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,20 @@
+//modal
 const modalOpen = document.querySelector('[data-modal]');
 
 if(modalOpen) {
 
   const modalId = modalOpen.getAttribute('data-modal');
-  const modalElem = document.getElementById(modalId)
+  const modalElem = document.getElementById(modalId);
+
   modalOpen.addEventListener('click', function() {
-    modalElem.classList.add("modal--open")
-  })
+    modalElem.classList.add("modal--open");
+  });
 
   modalElem.addEventListener('click', function(e) {
     if(e.target == modalElem) {
-      modalElem.classList.remove("modal--open")
-      console.log(123)
+      modalElem.classList.remove("modal--open");
     }
-
-  })
+  });
 }
 
 var parks = [
